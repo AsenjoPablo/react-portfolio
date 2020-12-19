a31f84f7c8736904e8f694294ac947589c6fd3f1
Pablo Asenjo <59542630+AsenjoPablo@users.noreply.github.com>
2020-12-19 21:17:53 +0100

    añadidos estilos home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,40 +2,59 @@ import "../styles/Home.css";
 
 import { Container, Row, Col } from "react-bootstrap";
 
+import avatar from "../img/avatar.png";
+
 // img imports
 
 const Home = () => {
   return (
-    <Container fluid id="main-home-container" className="m-0 p-0">
+    <Container
+      fluid
+      id="main-home-container"
+      className="m-0 p-0 animate__animated animate__fadeIn animate__faster"
+    >
       <Row>
-        <Col
-          lg={true}
-          className="animate__animated animate__fadeInLeft p-0 m-0"
-        >
-          <Row id="intro">
-            <div>
-              <h1>¡Hola, mundo!</h1>
-              <h3>
-                Soy Pablo, desarrollador{" "}
-                <span className="accent">FullStack</span>
-              </h3>
-            </div>
-          </Row>
-          <Row id="intro">
-            <div>
-              <h3>¿Qué te gustaría saber de mí?</h3>
-            </div>
-          </Row>
-          <div id="respuestas">
-            <Row id="respuesta-home">
+        <Col lg={true}>
+          <div id="preguntas" className="animate__animated animate__fadeInLeft">
+            <Row id="intro">
+              <div className="avatar-container">
+                <img src={avatar} className="avatar" />
+              </div>
               <div>
-                <h3>Quiero ver tus proyectos</h3>
+                <h1>¡Hola, mundo!</h1>
+                <h3>
+                  Soy Pablo, desarrollador{" "}
+                  <span className="accent">FullStack</span>
+                </h3>
               </div>
             </Row>
-            <Row id="respuesta-home">
-              <div>
-                <h3>Quiero ver tu currículum</h3>
+            <Row id="intro">
+              <div className="avatar-container">
+                <img src={avatar} className="avatar" />
               </div>
+              <div>
+                <h3>¿Qué te gustaría saber de mí?</h3>
+              </div>
+            </Row>
+          </div>
+
+          <div
+            id="respuestas"
+            className="animate__animated animate__fadeInRight"
+          >
+            <Row id="respuesta-home">
+              <a href="/proyectos">
+                <h3>
+                  Quiero ver tus proyectos <i className="fas fa-reply ml-3"></i>
+                </h3>
+              </a>
+            </Row>
+            <Row id="respuesta-home">
+              <a href="/proyectos">
+                <h3>
+                  Quiero ver tu currículum <i class="fas fa-reply ml-3"></i>
+                </h3>
+              </a>
             </Row>
           </div>
         </Col>
