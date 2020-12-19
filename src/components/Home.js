ed0324764655027383ae33e8b1c2b891c9352984
Pablo Asenjo <59542630+AsenjoPablo@users.noreply.github.com>
2020-12-19 12:54:14 +0100

    change

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
           </h2>
           <span className="orange">
             <em>
-              Welcome to my website rebuilt in ReactJS
+              Welcome to my website
               <br />
             </em>
           </span>
